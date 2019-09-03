b940c5ad283c6f1bb6f14f551f837f1f1236853d
tamiwang <tami.wang@outlook.com>
2019-09-03 16:57:29 +1000

    second commit

--- Selenium_Introduction/src/section9/Section9Assignment3.java
+++ Selenium_Introduction/src/section9/Section9Assignment3.java
@@ -1,9 +1,15 @@
 package section9;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Section9Assignment3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "/Users/admin/Downloads/chromedriver");
+		
+		WebDriver driver = new ChromeDriver();
+		
 
 	}
 
