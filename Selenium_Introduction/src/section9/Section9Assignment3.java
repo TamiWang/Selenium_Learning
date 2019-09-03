bbbc68c828b5d7f384b9ab398f6d8b34ddccb723
tamiwang <tami.wang@outlook.com>
2019-09-03 17:42:50 +1000

    Section9Assignment3-explicit wait

--- Selenium_Introduction/src/section9/Section9Assignment3.java
+++ Selenium_Introduction/src/section9/Section9Assignment3.java
@@ -1,7 +1,10 @@
 package section9;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Section9Assignment3 {
 
@@ -9,6 +12,18 @@ public class Section9Assignment3 {
 		System.setProperty("webdriver.chrome.driver", "/Users/admin/Downloads/chromedriver");
 		
 		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://www.itgeared.com/demo/1506-ajax-loading.html");
+		driver.findElement(By.partialLinkText("Click to load get data via Ajax!")).click();
+		
+		// explicit wait
+		// to target a specific element
+		WebDriverWait d = new WebDriverWait(driver, 20);
+		d.until(ExpectedConditions.visibilityOfElementLocated(By.id("results")));
+		
+		System.out.println(driver.findElement(By.id("results")).getText());
+		
+		
 		
 
 	}
