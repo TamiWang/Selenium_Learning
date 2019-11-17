8ef13d39b6ccef5e13ca449d708d95c2f9e988d6
tamiwang <tami.wang@outlook.com>
2019-11-18 10:59:17 +1100

    remove LIFX_Demo project for now

--- LIFX_Demo/src/mainpage/MainPageTest.java
+++
@@ -1,9 +0,0 @@
-package mainpage;
-
-public class MainPageTest {
-	
-	public void OpeningBrowser() {
-		System.out.println("aaa");
-	}
-
-}
--- Selenium_Introduction/src/section11/CalendarUI.java
+++ Selenium_Introduction/src/section11/CalendarUI.java
@@ -38,9 +38,7 @@ public class CalendarUI {
 				break;
 			}
 		}
-		
-		
-		
+
 	}
 
 }
