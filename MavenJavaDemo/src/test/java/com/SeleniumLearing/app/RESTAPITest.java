debbdba397553b7bc60d521155dde2c3b6de0970
tamiwang <tami.wang@outlook.com>
2019-12-12 10:52:58 +1100

    new project

---
+++ MavenJavaDemo/src/test/java/com/SeleniumLearing/app/RESTAPITest.java
@@ -0,0 +1,13 @@
+package com.SeleniumLearing.app;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	@Test
+	public void DeleteTwitter() {
+		System.out.println("DeleteTwitter");
+	}
+	
+
+}
---
+++ MavenJavaDemo/src/test/java/com/SeleniumLearing/app/SeleniumTest.java
@@ -0,0 +1,11 @@
+package com.SeleniumLearing.app;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void SeleniumLogin() {
+		System.out.println("SeleniumLogin");
+	}
+
+}
